4aaa24a2a0f4ad5e56eb6833177bc52a00944a25
JulesCocheme87 <jules87.cocheme@gmail.com>
2026-03-11 12:15:36 +0100

    Add files via upload

---
+++ importmap.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Returns the importmap for this application.
+ *
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ *
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ *
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
+return [
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    'vue' => [
+        'version' => '3.5.22',
+    ],
+    '@vue/runtime-dom' => [
+        'version' => '3.5.22',
+    ],
+    '@vue/runtime-core' => [
+        'version' => '3.5.22',
+    ],
+    '@vue/shared' => [
+        'version' => '3.5.22',
+    ],
+    '@vue/reactivity' => [
+        'version' => '3.5.22',
+    ],
+    'chart.js' => [
+        'version' => '4.5.0',
+    ],
+    '@symfony/ux-chartjs' => [
+        'version' => '2.30.0',
+    ],
+    '@kurkle/color' => [
+        'version' => '0.3.4',
+    ],
+    'react' => [
+        'version' => '18.3.1',
+    ],
+    'react-dom/client' => [
+        'version' => '18.3.1',
+    ],
+    'react-dom' => [
+        'version' => '18.3.1',
+    ],
+    'scheduler' => [
+        'version' => '0.23.2',
+    ],
+    '@symfony/ux-react' => [
+        'path' => './vendor/symfony/ux-react/assets/dist/loader.js',
+    ],
+];
